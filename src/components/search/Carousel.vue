84b54f3e5ea245cd5010603c9f03eb4caed7a4b1
knhmb <khaled@marvelconnect.com>
2022-01-20 16:31:41 +0800

    Completed the search page

---
+++ src/components/search/Carousel.vue
@@ -0,0 +1,149 @@
+<template>
+  <section class="search-carousel">
+    <el-carousel indicator-position="none" trigger="click" height="450px">
+      <el-carousel-item v-for="banner in banners" :key="banner.id">
+        <div class="image-content">
+          <img class="banner" :src="banner.banner" alt="" />
+          <h3>{{ banner.title }}</h3>
+          <p>{{ banner.description }}</p>
+        </div>
+      </el-carousel-item>
+    </el-carousel>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      banners: [
+        {
+          id: 1,
+          banner: require("../../assets/banner-CheungChau.jpg"),
+          title: "長洲",
+          description:
+            "長洲位於香港島之西南海域，面積約3平方公里人口雖只有3至4萬人，卻為本港各離島之冠。島民大部份世代而居，故相處融洽、民風簡樸、互助互愛。而且環境優美，空氣清新，設施齊備，交通便利，是香港人假日的好去處。",
+        },
+        {
+          id: 2,
+          banner: require("../../assets/banner-LantauIsland.jpg"),
+          title: "大嶼山",
+          description:
+            "大嶼山位於珠江口，是本港最大的一個島嶼，面積約144平方公里，近年因島上迪士尼樂園開幕而聞名中外。其實此島尚有很多知名的地方，如梅窩銀礦灣，大澳水鄉，寶蓮寺大佛及香港國際機場等均令各地遊人慕名而至。",
+        },
+        {
+          id: 3,
+          banner: require("../../assets/banner-LammaIsland.jpg"),
+          title: "南丫島",
+          description:
+            "南丫島位於香港島西南，面積約十三點六平方公里，是香港境內的第三大島嶼，由於島上風光明媚，有不少洋人聚居於此，令島上生活文化溶合了中西兩方面的特色。",
+        },
+        {
+          id: 4,
+          banner: require("../../assets/banner-FeaturedHotels.jpg"),
+          title: "香港渡假酒店之選",
+          description:
+            "吃會蝸皮里？動丁地車但雄洋玉良鼻。能久連風面犬現的林荷即帽菜圓飯書、六山坐女對進吃丟冰能；和唱幫小，己汁虎隻發土夏起習食自着至念耍燈；服幸何去足小詞身！愛寺完米，從少羊活娘葉找夏，明休且哥葉往。",
+        },
+        {
+          id: 5,
+          banner: require("../../assets/banner-DaytimeOvernight.jpg"),
+          title: "日間/晚間住宿",
+          description:
+            "吃會蝸皮里？動丁地車但雄洋玉良鼻。能久連風面犬現的林荷即帽菜圓飯書、六山坐女對進吃丟冰能；和唱幫小，己汁虎隻發土夏起習食自着至念耍燈；服幸何去足小詞身！愛寺完米，從少羊活娘葉找夏，明休且哥葉往。",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.search-carousel {
+  margin-bottom: 2.5rem;
+}
+
+.search-carousel .banner {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+}
+
+.search-carousel .image-content {
+  position: relative;
+}
+
+.search-carousel .image-content p,
+.search-carousel .image-content h3 {
+  position: absolute;
+  left: 4rem;
+  color: #fff;
+}
+
+.search-carousel .image-content h3 {
+  top: 60%;
+  font-size: 2rem;
+}
+
+.search-carousel .image-content p {
+  top: 75%;
+  max-width: 700px;
+}
+
+@media screen and (max-width: 1024px) {
+  .search-carousel .el-carousel .el-carousel__container {
+    height: 345px !important;
+  }
+}
+
+@media screen and (max-width: 820px) {
+  .search-carousel .el-carousel .el-carousel__container {
+    height: 260px !important;
+  }
+
+  .search-carousel .image-content p,
+  .search-carousel .image-content h3 {
+    left: 1rem;
+  }
+
+  .search-carousel .image-content h3 {
+    top: 45%;
+    /* font-size: 1rem; */
+  }
+
+  .search-carousel .image-content p {
+    top: 65%;
+    /* font-size: 0.6rem; */
+    /* margin-top: 0.5rem; */
+    max-width: 650px;
+  }
+}
+
+@media screen and (max-width: 540px) {
+  .search-carousel .el-carousel .el-carousel__container {
+    height: 140px !important;
+  }
+
+  .search-carousel .image-content p,
+  .search-carousel .image-content h3 {
+    left: 1rem;
+  }
+
+  .search-carousel .image-content h3 {
+    top: 30%;
+    font-size: 1rem;
+  }
+
+  .search-carousel .image-content p {
+    top: 45%;
+    font-size: 0.6rem;
+    margin-top: 0.5rem;
+  }
+}
+
+@media screen and (min-width: 1498px) {
+  .search-carousel .el-carousel .el-carousel__container {
+    min-height: 500px !important;
+  }
+}
+</style>
--- src/components/search/LeftSection.vue
+++ src/components/search/LeftSection.vue
@@ -1,27 +1,120 @@
 <template>
+  <Carousel />
   <base-container>
     <h2>渡假屋 <span>搜尋結果 :</span></h2>
-    <el-row justify="space-between">
-      <div class="left-section">
-        <el-col :sm="24" :lg="5">
+    <el-row :gutter="20">
+      <el-col :sm="24" :lg="6">
+        <div class="left-section">
           <p>Date</p>
           <Calendar />
-          <p>Time</p>
+          <!-- <el-row>
+            <el-col :span="12">
+              <p>Time</p>
+              <el-checkbox-group v-model="time">
+                <el-checkbox label="9:00 - 13:00" />
+                <el-checkbox label="13:00 - 21:00" />
+              </el-checkbox-group>
+            </el-col>
+            <el-col :span="12">
+              <p style="text-align: end">arrow</p>
+            </el-col>
+          </el-row> -->
+          <el-row>
+            <el-col>
+              <p>住宿要求</p>
+            </el-col>
+            <el-col :span="12">
+              <p>入住人數</p>
+            </el-col>
+            <el-col :span="12">
+              <div class="range">
+                <img class="plus-icon" src="../../assets/icon--.svg" alt="" />
+                <span>4</span>
+                <img class="plus-icon" src="../../assets/icon-+.svg" alt="" />
+              </div>
+            </el-col>
+
+            <el-col :span="12">
+              <p>房間</p>
+            </el-col>
+            <el-col :span="12">
+              <div class="range">
+                <img class="plus-icon" src="../../assets/icon--.svg" alt="" />
+                <span>2</span>
+                <img class="plus-icon" src="../../assets/icon-+.svg" alt="" />
+              </div>
+            </el-col>
+
+            <el-col :span="12">
+              <p>寵物</p>
+            </el-col>
+            <el-col :span="12">
+              <div class="pet-option">
+                <el-row>
+                  <el-col
+                    :class="{ 'is-active-option': isSelected === 'pet' }"
+                    :span="12"
+                  >
+                    <p @click="setOption('pet')">有</p>
+                  </el-col>
+                  <el-col
+                    :class="{ 'is-active-option': isSelected === 'no' }"
+                    :span="12"
+                  >
+                    <p @click="setOption('no')">沒有</p>
+                  </el-col>
+                </el-row>
+              </div>
+            </el-col>
+          </el-row>
+          <p>入住時段</p>
           <el-checkbox-group v-model="time">
             <el-checkbox label="9:00 - 13:00" />
             <el-checkbox label="13:00 - 21:00" />
           </el-checkbox-group>
-          <p>Location</p>
-          <el-checkbox-group v-model="location">
-            <el-checkbox label="location(3)" />
-            <el-checkbox label="location(8)" />
-            <el-checkbox label="location(12)" />
-            <el-checkbox label="location(10)" />
-            <el-checkbox label="location(24)" />
-            <el-checkbox label="location(25)" />
+          <p>地點</p>
+          <div class="location">
+            <el-checkbox-group v-model="location">
+              <el-checkbox label="長洲" />
+              <el-checkbox label="市政大樓" />
+              <el-checkbox label="東灣東堤" />
+              <el-checkbox label="西堤海景樓" />
+            </el-checkbox-group>
+          </div>
+          <div class="location">
+            <el-checkbox-group v-model="location">
+              <el-checkbox label="大嶼山" />
+              <el-checkbox label="梅窩" />
+              <el-checkbox label="貝澳" />
+              <el-checkbox label="塘福" />
+              <el-checkbox label="大澳" />
+            </el-checkbox-group>
+          </div>
+          <div class="location">
+            <el-checkbox-group v-model="location">
+              <el-checkbox label="南丫島" />
+              <el-checkbox label="模達灣" />
+              <el-checkbox label="沙埔舊村" />
+              <el-checkbox label="榕樹灣" />
+            </el-checkbox-group>
+          </div>
+          <div class="location">
+            <el-checkbox-group v-model="location">
+              <el-checkbox label="港島區" />
+              <el-checkbox label="九龍區" />
+              <el-checkbox label="新界區" />
+            </el-checkbox-group>
+          </div>
+          <p>房間類型</p>
+          <el-checkbox-group v-model="roomType">
+            <el-checkbox label="雙人套房" />
+            <el-checkbox label="家庭套房" />
+            <el-checkbox label="一房一廳" />
+            <el-checkbox label="兩房一廳" />
+            <el-checkbox label="三房/四房一廳" />
           </el-checkbox-group>
-        </el-col>
-      </div>
+        </div>
+      </el-col>
       <el-col :sm="24" :lg="18">
         <right-section></right-section>
       </el-col>
@@ -32,17 +125,26 @@
 <script>
 import Calendar from "./Calendar.vue";
 import RightSection from "./RightSection.vue";
+import Carousel from "./Carousel.vue";
 
 export default {
   components: {
     Calendar,
     RightSection,
+    Carousel,
   },
   data() {
     return {
       time: [],
       location: [],
+      isSelected: "pet",
+      roomType: [],
     };
+  },
+  methods: {
+    setOption(option) {
+      this.isSelected = option;
+    },
   },
 };
 </script>
@@ -65,6 +167,54 @@ export default {
 .search .left-section p {
   margin-top: 0.5rem;
   margin-bottom: 0.5rem;
+}
+
+.search .left-section .range {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+.search .left-section .range img {
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+}
+
+.search .left-section .range span {
+  margin: 0 1.9rem;
+}
+
+.search .left-section .pet-option {
+  background-color: #f5f5f5;
+  text-align: center;
+  border-radius: 18px;
+}
+
+.search .left-section .pet-option .el-col {
+  border-radius: 18px;
+}
+
+.search .left-section .pet-option .el-col p {
+  margin: 0;
+  padding: 0.3rem 0;
+  color: #999;
+  cursor: pointer;
+}
+
+.search .is-active-option {
+  background-color: #fd9a1a;
+}
+
+.search .left-section .pet-option .el-col.is-active-option p {
+  color: #fff;
+}
+
+.search .left-section .location:not(:last-of-type) {
+  border-bottom: 1px solid #d8d8d8;
+  padding-bottom: 0.5rem;
 }
 
 .search .left-section .el-checkbox--default {
